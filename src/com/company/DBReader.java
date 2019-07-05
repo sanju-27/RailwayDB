88e66b0b37ec61294f56160ce8ccb93aef887507
sanju-27 <sanjaystz98@gmail.com>
2019-07-05 15:22:23 +0530

    Update DBReader.java

--- src/com/company/DBReader.java
+++ src/com/company/DBReader.java
@@ -12,74 +12,69 @@ public class DBReader {
     private static String viewUser = "SELECT * FROM user WHERE uid =?";
     private static String viewTrain = "SELECT * FROM train";
     private static String countTrain = "SELECT trainno FROM train";
-    Connection con;
-    BufferedReader br;
+    private Connection con;
+    private BufferedReader br;
 
-    public DBReader() {
-        try{
+    DBReader() {
+        try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             this.con = DriverManager.getConnection(dbURL, dbUser, dbPwd);
             this.br = new BufferedReader(new InputStreamReader(System.in));
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
+        } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public int countUser() {
+    int countUser() {
         int count = 0;
         try {
             PreparedStatement ps = con.prepareStatement(countUser);
             ResultSet rs = ps.executeQuery();
-            count = rs.getRow();
+//            count = rs.getRow();
+            while (rs.next())
+                count++;
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
         return count;
     }
 
-    public void viewUser(int id) {
+    void viewUser(int id) {
         try {
             PreparedStatement ps = con.prepareStatement(viewUser);
-            ps.setInt(1,id);
+            ps.setInt(1, id);
             ResultSet rs = ps.executeQuery();
-            if(rs.next())
-            {
-                System.out.println("UID: "+rs.getInt(1)+" Name: "+rs.getString(2)+"\nPhone: "+rs.getString(4)+" Age: "+rs.getInt(5));
-            }
-            else
+            if (rs.next()) {
+                System.out.println("UID: " + rs.getInt(1) + " Name: " + rs.getString(2) + "\nPhone: " + rs.getString(4) + " Age: " + rs.getInt(5));
+            } else
                 System.out.println("User not found");
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
     }
-    public void viewUser()
-    {
+
+    void viewUser() {
         try {
             PreparedStatement ps = con.prepareStatement(countUser);
             ResultSet rs = ps.executeQuery();
-            while(rs.next())
-            {
-                System.out.println(rs.getInt(1)+". "+rs.getString(2));
+            while (rs.next()) {
+                System.out.println(rs.getInt(1) + ". " + rs.getString(2));
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public void viewTrains() {
+    void viewTrains() {
         try {
             PreparedStatement ps = con.prepareStatement(viewTrain);
             ResultSet rs = ps.executeQuery();
-            while(rs.next())
-            {
-                System.out.println("Train No.: "+rs.getInt(1));
+            while (rs.next()) {
+                System.out.println("Train No.: " + rs.getInt(1));
                 System.out.println("Route:");
-                System.out.println("From: "+rs.getString(2)+" To: "+rs.getString(3));
-                System.out.println("Working Days:\n"+rs.getString(11));
+                System.out.println("From: " + rs.getString(2) + " To: " + rs.getString(3));
+                System.out.println("Working Days:\n" + rs.getString(11));
                 System.out.println("------------------------------------------------------------");
             }
         } catch (SQLException e) {
@@ -87,12 +82,13 @@ public class DBReader {
         }
     }
 
-    public int countTrain() {
+    int countTrain() {
         int count = 0;
         try {
             PreparedStatement ps = con.prepareStatement(countTrain);
             ResultSet rs = ps.executeQuery();
-            count = rs.getRow();
+            while (rs.next())
+                count++;
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -100,58 +96,65 @@ public class DBReader {
         return count;
     }
 
-    public boolean checkPass(int id, String pwd) {
+    boolean checkPass(int id, String pwd) {
 
-        try
-        {
+        try {
             PreparedStatement ps = con.prepareStatement(viewUser);
-            ps.setInt(1,id);
+            ps.setInt(1, id);
             ResultSet rs = ps.executeQuery();
-            if(rs.next())
-            {
-                if(pwd.equals(rs.getString(3)))
-                    return true;
-                else
-                    return false;
+            if (rs.next()) {
+                return pwd.equals(rs.getString(3));
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-            return false;
+        return false;
     }
 
-    public void viewPNR(int id) {
+    void viewPNR(int id) {
         try {
             PreparedStatement ps = con.prepareStatement("SELECT * FROM ticket WHERE uid = ?");
-            ps.setInt(1,id);
+            ps.setInt(1, id);
             ResultSet rs = ps.executeQuery();
             ps = con.prepareStatement("SELECT * FROM user WHERE uid = ?");
-            ps.setInt(1,id);
+            ps.setInt(1, id);
             ResultSet rs2 = ps.executeQuery();
             rs2.next();
-            while(rs.next())
-            {
+            while (rs.next()) {
 
-                System.out.println("PNR Number: "+rs.getInt(1));
+                System.out.println("PNR Number: " + rs.getInt(1));
 //                if(!rs2.next())
-                    System.out.println("User Name: "+rs2.getString(2));
-                System.out.println("Number of Passengers: "+rs.getInt(5)+"\tBill: "+rs.getString(6));
+                System.out.println("User Name: " + rs2.getString(2));
+                System.out.println("Number of Passengers: " + rs.getInt(5) + "\tBill: " + rs.getString(6));
                 ps = con.prepareStatement("SELECT * FROM train_status WHERE statno = ?");
                 ps.setInt(1, rs.getInt("statno"));
                 ResultSet rs3 = ps.executeQuery();
-                if(rs3.next())
-                {
-                    System.out.println("Date: "+rs3.getString(2));
+                if (rs3.next()) {
+                    System.out.println("Date: " + rs3.getString(2));
                     ps = con.prepareStatement("SELECT * FROM train WHERE trainno = ?");
-                    ps.setInt(1,rs3.getInt("trainno"));
+                    ps.setInt(1, rs3.getInt("trainno"));
                     ResultSet rs4 = ps.executeQuery();
-                    if(rs4.next())
-                        System.out.println("From: "+rs4.getString(2)+"\tTo: "+rs4.getString(3));
+                    if (rs4.next())
+                        System.out.println("From: " + rs4.getString(2) + "\tTo: " + rs4.getString(3));
                 }
+                System.out.println("-----------------------------------");
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
+    }
+
+    String viewName(int id) {
+        String s = "";
+        try {
+            PreparedStatement ps = con.prepareStatement("SELECT name FROM user WHERE uid = "+id);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next())
+                s = rs.getString(1);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return s;
     }
 }
