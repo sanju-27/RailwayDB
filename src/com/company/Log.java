eba390457dd01244f6f5425092683021aab2e6dd
sanju-27 <sanjaystz98@gmail.com>
2019-07-05 15:22:29 +0530

    Update Log.java

--- src/com/company/Log.java
+++ src/com/company/Log.java
@@ -5,18 +5,18 @@ import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
-public class Log {
+class Log {
 
     void log(boolean x, String msg) throws IOException {
-        Logger logger = Logger.getLogger("BusReservationSystem");
-        FileHandler fh = new FileHandler("C:\\Users\\Administrator\\Desktop\\Reservation\\logs\\Logs");
+        Logger logger = Logger.getLogger("RailwayDB");
+        FileHandler fh = new FileHandler("C:\\Users\\Administrator\\Desktop\\Reservation\\logs\\Logs.txt",true);
         logger.addHandler(fh);
         SimpleFormatter formatter = new SimpleFormatter();
         fh.setFormatter(formatter);
         logger.setUseParentHandlers(false);
-        if(!x)
-            logger.info(msg);
+        if (!x)
+            logger.info(msg+"\n---X---\n");
         else
-            logger.warning(msg);
+            logger.warning(msg+"\n---X---\n");
     }
 }
